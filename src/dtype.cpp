acfe96a2be7d23b7688c927e1e4d983b7d8851d1
pepijn-devries <peppi2@gmail.com>
2025-08-21 00:22:25 +0200

    if

--- src/dtype.cpp
+++ src/dtype.cpp
@@ -492,8 +492,8 @@ bool convert_data(uint8_t *input, int rtype, int n,
         // if (!ignore_na && ((int *)input)[i] == (0xff & INTEGER(na_value)[0]))
         //   warn_na = true;
         Rprintf("TODO typeof new na %i\n", TYPEOF(new_na_value));
-        bool it = (((int *)input)[i] == (0xff & INTEGER(new_na_value)[0]));
-        Rprintf("TODO typeof new na %i %i\n", TYPEOF(new_na_value), it);
+        if (((int *)input)[i] == (0xff & INTEGER(new_na_value)[0]))
+          Rprintf("Ja");
 
       } else {
         stop("Failed to convert data");
