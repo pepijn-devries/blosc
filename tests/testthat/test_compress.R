ad6585a3c1713c05261cf63407890d60e0ab0c0f
pepijn-devries <peppi2@gmail.com>
2025-08-20 19:44:38 +0200

    remove test for debugging purposes

--- tests/testthat/test_compress.R
+++
@@ -1,8 +0,0 @@
-test_that("Data can be compressed and decompressed", {
-  expect_true({
-    volcano_compressed <- blosc_compress(volcano, typesize = 2, dtype = "<f2")
-    volcano_reconstruct <- blosc_decompress(volcano_compressed, dtype = "<f2")
-    object.size(volcano) > object.size(volcano_compressed) &&
-      all(volcano_reconstruct == volcano)
-  })
-})
